6557541e3b14294b7ab5e51ff163d3018406e042
Z-e-Games <93876011+Z-e-Games@users.noreply.github.com>
2022-02-11 20:11:35 -0600

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -4,26 +4,31 @@ const diamondPlace = document.getElementById('diamond')
 const skinPlace = document.getElementById('skin-show')
 const xpPlace = document.getElementById('xp-show')
 const levelPlace = document.getElementById('level-show')
-
+if(localStorage.getItem('pulse ammo') === null){
+    localStorage.setItem('pulse ammo',100)
+}
+if(localStorage.getItem('pulse ammo') === null){
+    localStorage.setItem('pulse ammo',100)
+}
 if(localStorage.getItem('diamonds') === null){
     localStorage.setItem('diamonds',100)
 }
-if(localStorage.getItem('gold coins') === null){
-    localStorage.setItem('gold coins', 100)
+if(localStorage.getItem('tech coins') === null){
+    localStorage.setItem('tech coins', 100)
 }
 
-if(localStorage.getItem('has elf skin') === null){
-    localStorage.setItem('has elf skin', 'no')
+if(localStorage.getItem('has season skin') === null){
+    localStorage.setItem('has season skin', 'no')
 }
 
 if(localStorage.getItem('hasSquidGameMusic') === null){
     localStorage.setItem('hasSquidGameMusic','no')
 }
 if(localStorage.getItem('is signed in') === 'yes'){
-    goldCoinsHtml.innerHTML = `Gold Coins: ${localStorage.getItem('gold coins')}`  
+    goldCoinsHtml.innerHTML = `Gold Coins: ${localStorage.getItem('tech coins')}`  
     diamondPlace.innerHTML = `Diamonds: ${localStorage.getItem('diamonds')}`
     usernamePlace.innerHTML = localStorage.getItem('username')
-    xpPlace.innerHTML = `XP: ${localStorage.getItem('XP')}/3500`
+    xpPlace.innerHTML = `XP: ${localStorage.getItem('XP')}/20000`
     levelPlace.innerHTML = `Level: ${localStorage.getItem('Level')}`
 }else{
     goldCoinsHtml.innerHTML = `Gold Coins: 0`
@@ -31,7 +36,7 @@ if(localStorage.getItem('is signed in') === 'yes'){
 }
 
 
-if(localStorage.getItem('has elf skin') === 'yes'){
+if(localStorage.getItem('has season skin') === 'yes'){
     skinPlace.innerHTML = `Skin: ELF`
 }
  
@@ -50,11 +55,11 @@ if(localStorage.getItem('XP') === null){
     localStorage.setItem('XP', 0)
 }
 
-if(localStorage.getItem('XP') >= 3500){
-    localStorage.setItem('XP', parseInt(localStorage.getItem('XP')) - 3500)
+if(localStorage.getItem('XP') >= 2000){
+    localStorage.setItem('XP', parseInt(localStorage.getItem('XP')) - 2000)
     localStorage.setItem('Level',parseInt(localStorage.getItem('Level')) + 1)
-    localStorage.setItem('gold coins', parseInt(localStorage.getItem('gold coins')) + 8500)
-    localStorage.setItem('has elf skin', 'yes')
+    localStorage.setItem('tech coins', parseInt(localStorage.getItem('tech coins')) + 8500)
+    localStorage.setItem('has season skin', 'yes')
     location.reload()
 }
 
@@ -65,6 +70,7 @@ if(localStorage.getItem('hasSquidGameMusic') === null){
 }
 
 if(localStorage.getItem('username') === 'Hardik' && localStorage.getItem('**') === 'adminCode(BREME)'){
-    localStorage.setItem('gold coins', 100000000000)
+    localStorage.setItem('tech coins', 100000000000)
     localStorage.setItem('diamonds',1000000000000)
 }
+
